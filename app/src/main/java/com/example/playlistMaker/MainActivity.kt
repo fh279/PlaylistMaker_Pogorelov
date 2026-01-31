62d28071954bf2c1a5efe0a43787063ad95e11e6
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2026-01-31 18:58:45 +0300

    Add toasts for buttons in Main Screen. Add corresponding string resources

--- app/src/main/java/com/example/playlistMaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistMaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistMaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +12,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         enableEdgeToEdge()
+
+        val searchButton = findViewById<Button>(R.id.main_screen_search_button)
+        val mediaButton = findViewById<Button>(R.id.main_screen_media_button)
+        val settingsButton = findViewById<Button>(R.id.main_screen_settings_button)
+
+
+        // Way 1. Implementation of anonymous object
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    R.string.media_button_toast_text,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        searchButton.setOnClickListener(imageClickListener)
+
+
+        // Way 2. Implementation of lambda expression
+        mediaButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.media_button_toast_text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.settings_button_toast_text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
