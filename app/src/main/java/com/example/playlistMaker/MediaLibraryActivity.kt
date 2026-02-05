57ee3842f070fe408383712d5c6203957d356a94
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2026-02-05 17:40:06 +0300

    Add templates for MediaLibrary and Search activities

---
+++ app/src/main/java/com/example/playlistMaker/MediaLibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlistMaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MediaLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medialibrary)
+    }
+}
