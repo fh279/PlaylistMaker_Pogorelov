0c418c2b94ad86f2fcbb0dab026b0c0edaa26955
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2026-02-06 17:45:45 +0300

    Edit Activities layout settings, make non-main activities non-exportable

--- app/src/main/java/com/example/playlistMaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistMaker/SearchActivity.kt
@@ -1,11 +1,13 @@
 package com.example.playlistMaker
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        enableEdgeToEdge()
     }
 }
