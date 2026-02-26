dcbd9d9db414967936fd5d7f4dca67a9541356af
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2026-02-26 13:56:19 +0300

    Fix hiding keyboard

--- app/src/main/java/com/example/playlistMaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistMaker/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistMaker
 
+import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
@@ -12,7 +13,6 @@ import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,40 +21,27 @@ class SearchActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         val toolbar = findViewById<MaterialToolbar>(R.id.search_screen_toolbar)
-
-        toolbar.setNavigationOnClickListener {
-            finish()
-        }
+        toolbar.setNavigationOnClickListener { finish() }
 
         val clearButton = findViewById<ImageButton>(R.id.clearButton)
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
 
         searchEditText.addTextChangedListener(
             object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                     clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
                 }
 
-                override fun afterTextChanged(s: Editable?) {
-                }
+                override fun afterTextChanged(s: Editable?) {}
             }
         )
 
         clearButton.setOnClickListener {
             searchEditText.text.clear()
             searchEditText.clearFocus()
-            if (!searchEditText.isFocused) {
-                val imm = this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
-            }
+            hideKeyboard(searchEditText)
         }
     }
 
@@ -68,8 +55,15 @@ class SearchActivity : AppCompatActivity() {
                     currentFocus.clearFocus()
                     hideKeyboard(currentFocus)
                 }
+            } else if (currentFocus != null) {
+                hideKeyboard(currentFocus)
             }
         }
         return super.dispatchTouchEvent(ev)
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
